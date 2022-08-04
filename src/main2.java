1968f30b7d6d9f9060734e27bb2eb977d66b93f5
nurislam09 <110288200+nurislam09@users.noreply.github.com>
2022-08-04 13:18:06 +0600

    Update main2.java

--- src/main2.java
+++ src/main2.java
@@ -7,10 +7,12 @@ public class main2 {
         car.volume = 3.0;
         car.year= 2022;
         car.color= "Light Green";
+        car.salon="White"
         System.out.println(car.model);
         System.out.println(car.price);
         System.out.println(car.placeOfProduction);
         System.out.println(car.volume);
         System.out.println(car.year);
+        System.out.println(car.salon);  
     }
 }
